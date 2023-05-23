d0caba4d0ae226e27587977e69bc25cac338a4b5
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-05-23 14:05:39 +0300

    Create Food.java

---
+++ P03WildFarm/Food.java
@@ -0,0 +1,11 @@
+package P03WildFarm;
+
+public abstract class Food {
+    private Integer quantity;
+    public Food(Integer quantity) {
+        this.quantity = quantity;
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+}

9e33c1f30d83d46d5245a563e6acef7defb667b3
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-05-23 14:05:34 +0300

    Create Mammal.java

---
+++ P03WildFarm/Mammal.java
@@ -0,0 +1,7 @@
+package P03WildFarm;
+
+public abstract class Mammal extends Animal{
+    public Mammal(String name, String type, double weight, String livingRegion) {
+        super(name, type, weight, livingRegion);
+    }
+}

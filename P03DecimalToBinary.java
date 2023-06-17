74b2400884c38872cdcf5a62d3437cf0306fb313
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-06-17 11:00:39 +0300

    Create P03DecimalToBinary.java

---
+++ P03DecimalToBinary.java
@@ -0,0 +1,26 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class P03DecimalToBinary {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        if (number == 0) {
+            stack.push(0);
+
+        }else{
+            while (number > 0) {
+
+                stack.push(number % 2);
+                number = number / 2;
+            }
+        }
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+    }
+}

946828b5950e96d3103f877a6880ffa8f26b95fe
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2024-03-11 22:04:04 +0200

    Create TableRepositoryImpl.java

---
+++ restaurant/repositories/TableRepositoryImpl.java
@@ -0,0 +1,34 @@
+package restaurant.repositories;
+
+import restaurant.entities.tables.interfaces.Table;
+import restaurant.repositories.interfaces.TableRepository;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class TableRepositoryImpl implements TableRepository<Table> {
+    private Collection<Table> entities;
+
+    public TableRepositoryImpl() {
+        this.entities = new ArrayList<>();
+    }
+
+    @Override
+    public Collection<Table> getAllEntities() {
+        return Collections.unmodifiableCollection(this.entities);
+    }
+
+    @Override
+    public void add(Table entity) {
+       this.entities.add(entity);
+    }
+
+    @Override
+    public Table byNumber(int number) {
+        return this.entities.stream()
+                .filter(e->e.getTableNumber() == number)
+                .findFirst()
+                .orElse(null);
+    }
+}
